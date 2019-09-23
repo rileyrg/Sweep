518074e620791134c2ac43a7b069a01a8160569c
Richard Riley <rileyrg@gmail.com>
2019-09-23 23:43:58 +0200

    added Sweep.h as a test of ccls nd pio build path

---
+++ include/Sweep.h
@@ -0,0 +1,5 @@
+#include<Arduino.h>
+
+#include<Servo.h>
+
+#define STEPLIMIT 150
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,4 @@
-
-#include<Arduino.h>
-
-#include<Servo.h>
+#include<Sweep.h>
 
 Servo myservo;  // create servo object to control a servo
 
@@ -15,12 +12,12 @@ void setup()
 
 void loop()
 {
-  for (pos = 0; pos <= 150; pos += 1) { // goes from 30 degrees to 150 degrees
+  for (pos = 0; pos <= STEPLIMIT; pos += 1) { // goes from 30 degrees to 150 degrees
     // in steps of 1 degree
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
     delay(20);                       // waits 15ms for the servo to reach the position
   }
-  for (pos =150; pos >= 0; pos -= 1) { // goes from 150 degrees to 30 degrees
+  for (pos =STEPLIMIT; pos >= 0; pos -= 1) { // goes from 150 degrees to 30 degrees
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
     delay(20);                       // waits 15ms for the servo to reach the position
   }
