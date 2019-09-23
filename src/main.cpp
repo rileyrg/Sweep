6f1633fe9e59c99ce2c6a7c377b4c370fc9877b0
Richard Riley <rileyrg@gmail.com>
2019-09-23 22:15:56 +0200

    added servo library.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
+
 #include<Arduino.h>
-#include <Servo.h>
+
+#include<Servo.h>
+
 Servo myservo;  // create servo object to control a servo
+
 // twelve servo objects can be created on most boards
 
 int pos = 0;    // variable to store the servo position
